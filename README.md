1865d66595d100bf941c6bbe9c9081f1e34e9440
ponbiki <jcandroscavage@hotmail.com>
2016-10-12 02:17:58 -0400

    setup

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on October 12, 2016, 2:04 am.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ponbiki
+ * Date: 10/12/16
+ * Time: 2:08 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+class GenusController
+{
+    /**
+     * @Route
+     */
+    public function showAction()
+    {
+
+    }
+}
